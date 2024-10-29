5a586f3f6aeb11e91d169776128dd938356c9557
ALAN PEÑIÑOREY <gapgonza@gmail.com>
2024-10-29 19:12:46 -0300

    actualizacion host

--- models/db.js
+++ models/db.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require('sequelize');
 
 // Crear la instancia de Sequelize
-const sequelize = new Sequelize('agendamedica', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql', 
+const sequelize = new Sequelize('bmlvaummwdviifhwbgyw', 'uipshsw3btzo3lbt', 'vYYqjF8pUOhmWTBKlVtQ', {
+    host: 'bmlvaummwdviifhwbgyw-mysql.services.clever-cloud.com',
+    dialect: 'mysql',
+    port: 3306
 });
 
 module.exports = sequelize;
